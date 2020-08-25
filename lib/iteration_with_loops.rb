155ec8fbe2eac3790c1e60604831b72d296cabca
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 23:48:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,10 +8,14 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.length do
     element_index = 0
+    lowest_temp = 
     while element_index < src[row_index].length do
-      
+      if src[row_index][element_index] < lowest_temp
+        lowest_temp = src[row_index][element_index]
+      end
       element_index += 1
     end
+    min_each_day << lowest_temp
     row_index += 1
   end
 end
